7f2280043daa686b65739b6f5a834339fde65014
LionelKarlen <lionel.karlen@protonmail.com>
2021-12-19 21:00:35 +0100

    feat: Add clients search

--- packages/renderer/src/views/Clients.svelte
+++ packages/renderer/src/views/Clients.svelte
@@ -3,6 +3,7 @@
   import ipc from "../services/ipcService";
   import { onMount } from "svelte";
   import {
+    Search,
     StructuredList,
     StructuredListCell,
     StructuredListHead,
@@ -12,23 +13,32 @@
   import page from "page";
 
   let clients: Client[] = [];
+  let filteredClients: Client[] = [];
+  let searchValue: string = "";
 
   onMount(() => getData());
   async function getData() {
     clients = await ipc.invoke("getClients");
+    filteredClients = clients;
   }
-  $: console.log(clients);
+  function filterClients(search: string) {
+    filteredClients = clients.filter((value: Client) => {
+      return value.name.includes(search);
+    });
+  }
+  $: filterClients(searchValue);
 </script>
 
 <h1>Clients</h1>
+<Search bind:value={searchValue} />
 {#if clients.length > 0}
-  <StructuredList condensed>
+  <StructuredList condensed selection>
     <StructuredListHead>
       <StructuredListRow head>
         <StructuredListCell head>Last name</StructuredListCell>
         <StructuredListCell head>First name</StructuredListCell>
       </StructuredListRow>
-      {#each clients as client}
+      {#each filteredClients as client}
         <StructuredListRow on:click={() => page(`/client/${client.id}`)}>
           <StructuredListCell
             >{client.name.split(" ")[
