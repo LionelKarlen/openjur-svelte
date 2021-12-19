30688054999437a87deeff5527811b03c9037c17
LionelKarlen <lionel.karlen@protonmail.com>
2021-12-19 18:47:02 +0100

    ci: Fix build setup

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -3,6 +3,18 @@
  * @see https://www.electron.build/configuration/configuration
  */
 module.exports = {
+  appId: "org.suspiciouslycool.openjur",
+  productName: "OpenJUR",
+  win: {
+    target: "nsis",
+  },
+  linux: {
+    target: "AppImage",
+  },
+  nsis: {
+    allowToChangeInstallationDirectory: true,
+    oneClick: false,
+  },
   directories: {
     output: "dist",
     buildResources: "buildResources",
